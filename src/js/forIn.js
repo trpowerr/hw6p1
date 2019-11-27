f0e54613c13ab1005172c1b3619aa3e7ae0bbabd
trpowerr <48128809+trpowerr@users.noreply.github.com>
2019-11-27 22:58:23 +0300

    Update forIn.js

--- src/js/forIn.js
+++ src/js/forIn.js
@@ -1,20 +1,29 @@
 const obj = {name: 'мечник', health: 10, level: 2, attack: 80, defence: 40}
-let orderArr = {};
+let orderArr = [];
+let finalArr = {};
 
 function orderByProps(obj, arr) {
     for (let property in obj) {
       for (let prop in arr) {
           let item = arr[prop];
           if (property == item) {
-            orderArr[property] = obj[property];
+            finalArr.key = property;
+            console.log(typeof finalArr.key);
+            finalArr.value = obj[property];
+            console.log(typeof obj[property]);
+            console.log(finalArr);
+            // orderArr.push(finalArr);
+            // console.log(orderArr);
             delete obj[property];
           } 
         }
     }
+    
 
-    obj = Object.assign(orderArr, obj);
+    obj = Object.assign(finalArr, obj);
+
     return obj;
 
 }    
 
-console.log(orderByProps(obj, ['health','defence']));
+console.log(orderByProps(obj, ['name','level']));
